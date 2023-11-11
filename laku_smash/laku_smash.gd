6fb951cd8c8df24b6519ffdb0111c6d34dd5df53
tatu_heinamaki <tatu.heinamaki@gmail.com>
2023-11-11 14:44:34 +0200

    Updating the graphics. Few new animations.

--- laku_smash/laku_smash.gd
+++ laku_smash/laku_smash.gd
@@ -13,17 +13,31 @@ func _ready():
 	for piece in used_pieces:
 		$grid.possible_pieces.append(piece)
 	$grid.spawn_pieces()
+	$laku_curtains.get_node("curtains_animation").connect("animation_finished", self, "_on_curtains_animation_animation_finished")
+	$laku_curtains.get_node("curtains_animation").play("curtains_open")
 
 
 func _on_move_keeper_moves_deplenished():
-	used_pieces.clear()
-	for piece in possible_pieces:
-		used_pieces.append(piece)
-	used_pieces.remove(randi() % used_pieces.size())
-	used_pieces.append(sydan_piece)
-	$grid.possible_pieces.clear()
-	for piece in used_pieces:
-		$grid.possible_pieces.append(piece)
-	$grid.reset()
-	$score_keeper.reset_score()
-	$move_keeper.reset_moves()
+	$end_delay.start()
+	$grid.can_play = false
+	
+
+func _on_curtains_animation_animation_finished(anim_name):
+	if anim_name == "curtains_close":
+		used_pieces.clear()
+		for piece in possible_pieces:
+			used_pieces.append(piece)
+		used_pieces.remove(randi() % used_pieces.size())
+		used_pieces.append(sydan_piece)
+		$grid.possible_pieces.clear()
+		for piece in used_pieces:
+			$grid.possible_pieces.append(piece)
+		$grid.reset()
+		$score_keeper.reset_score()
+		$move_keeper.reset_moves()
+		$grid.can_play = true
+		$laku_curtains.get_node("curtains_animation").play("curtains_open")
+
+
+func _on_end_delay_timeout():
+	$laku_curtains.get_node("curtains_animation").play("curtains_close")
---
+++ laku_smash/laku_smash_menu.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+# alempi laku pos = 103,369
+# ylmepi laku pos = 194,261.5
+
+func _on_start_game_pressed():
+	print("start")
+	$laku_curtains.get_node("curtains_animation").connect("animation_finished", self, "_on_curtains_animation_animation_finished")
+	$laku_curtains.get_node("curtains_animation").play("curtains_close")
+
+
+func _on_start_delay_timeout():
+	$bg/logo_animation.play("laku_smashing")
+
+
+func _on_curtains_animation_animation_finished(anim_name):
+	get_tree().change_scene("res://laku_smash/laku_smash.tscn")
